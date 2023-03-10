4942225eb3a251763eb557c6fcca83ddaa96443f
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-03-10 14:35:33 +0100

    Connect products api

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Item = () => {
     const { id } = useParams();
-    return <div>Item {id}</div>;
+    const shopUrl = "https://fakestoreapi.com/products/" + id;
+    const [item, setItem] = useState({});
+
+    useEffect(() => {
+        const getItem = async () => {
+            const product = await fetch(shopUrl).then((result) =>
+                result.json()
+            );
+            setItem(product);
+        };
+        getItem();
+    }, [shopUrl]);
+
+    return <div>{item.title}</div>;
 };
 
 export default Item;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,15 +1,60 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "./Items.css";
 
 const Items = () => {
-    const items = [1, 2, 3, 4, 5, 6, 7, 8];
-    return items.map((item) => {
+    const shopUrl = "https://fakestoreapi.com/products";
+
+    const [items, setItems] = useState([]);
+    const [amount, setAmount] = useState(6);
+
+    useEffect(() => {
+        const getItems = async () => {
+            let query = "";
+            if (amount) {
+                query = `?limit=${amount}`;
+            }
+            const products = await fetch(shopUrl + query).then((result) =>
+                result.json()
+            );
+            setItems(products);
+        };
+        getItems();
+    }, [amount]);
+
+    const loadMore = () => {
+        setAmount((prevAmount) => prevAmount + 3);
+    };
+
+    const products = items.map((item) => {
         return (
-            <div key={item}>
-                <Link to={`/items/${item}`}>Item {item}</Link>
+            <div key={item.id}>
+                <Link to={`/items/${item.id}`}>{item.title}</Link>
                 <br />
             </div>
         );
     });
+
+    return (
+        <div className="items">
+            {products}
+            {amount < 20 && <LoadMoreButton loadMore={loadMore} />}
+        </div>
+    );
+};
+
+const LoadMoreButton = ({ loadMore }) => {
+    return (
+        <div className="load-more">
+            <button className="button" onClick={loadMore}>
+                Load more..
+            </button>
+        </div>
+    );
+};
+
+const ItemCard = () => {
+    return <div>Hei</div>;
 };
 
 export default Items;
