8f76a08b92e7565c2e6becb79d6615fb4de957ef
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-03-14 15:44:54 +0100

    Set category from url

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,20 +1,67 @@
 import { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useParams } from "react-router-dom";
 import "./Items.css";
 import urlSlug from "url-slug";
 
 const Items = () => {
     const shopUrl = "https://fakestoreapi.com/products";
+    const urlParams = useParams();
+
+    const CATEGORIESTEMPLATE = {
+        name: "all items",
+        slug: urlSlug("all items"),
+    };
 
     const [items, setItems] = useState([]);
     const [amount, setAmount] = useState(6);
-    const [category, setCategory] = useState("all items");
+    const [currentCategory, setCurrentCategory] = useState(CATEGORIESTEMPLATE);
+    const [allCategories, setAllCategories] = useState([CATEGORIESTEMPLATE]);
     const [displayLoadButton, setDisplayLoadButton] = useState(true);
+    const [shouldFetchItems, setShouldFetchItems] = useState(false);
 
+    // On mount, fetch categories
     useEffect(() => {
+        const getCategories = async () => {
+            const response = await fetch(shopUrl + "/categories").then((res) =>
+                res.json()
+            );
+            const categories = response.map((cat) => {
+                return {
+                    name: cat,
+                    slug: urlSlug(cat),
+                };
+            });
+            setAllCategories([CATEGORIESTEMPLATE, ...categories]);
+        };
+        getCategories();
+    }, []);
+
+    // Get current category from URL
+    useEffect(() => {
+        const setCategoryFromUrl = () => {
+            const slug = urlParams.category;
+            let category;
+            for (let cat of allCategories) {
+                if (cat.slug === slug) {
+                    category = cat;
+                }
+            }
+            if (category) {
+                setCurrentCategory(category);
+                setShouldFetchItems(true);
+            }
+        };
+        setCategoryFromUrl();
+    }, [allCategories, urlParams.category]);
+
+    // Fetch items
+    useEffect(() => {
+        // fetch every time, or get all item once?
         const getItems = async () => {
             const categoryQuery =
-                category === "all items" ? "" : "/category/" + category;
+                currentCategory.name === CATEGORIESTEMPLATE.name
+                    ? ""
+                    : "/category/" + currentCategory.name;
             let limitQuery = "";
             if (amount) {
                 limitQuery = `?limit=${amount}`;
@@ -28,14 +75,20 @@ const Items = () => {
                 setDisplayLoadButton(true);
             }
             setItems(products);
+            console.log(currentCategory);
+            console.log("products fetched");
         };
-        getItems();
-    }, [amount, category]);
+        if (shouldFetchItems) {
+            getItems();
+        }
+    }, [amount, currentCategory, shouldFetchItems, CATEGORIESTEMPLATE.name]);
 
     const loadMore = () => {
         setAmount((prevAmount) => prevAmount + 3);
         setTimeout(() => {
-            const newItems = document.querySelector(".product-card:nth-last-child(6)");
+            const newItems = document.querySelector(
+                ".product-card:nth-last-child(6)"
+            );
             newItems.scrollIntoView();
         }, 250);
     };
@@ -46,7 +99,7 @@ const Items = () => {
 
     return (
         <div className="product-page">
-            <ItemMenu onClick={setCategory} />
+            <ItemMenu onClick={setCurrentCategory} categories={allCategories} />
             <div className="items">{products}</div>
             {displayLoadButton && <LoadMoreButton loadMore={loadMore} />}
         </div>
@@ -81,30 +134,20 @@ const ItemCard = ({ item }) => {
 };
 
 // ITEM MENU
-const ItemMenu = ({ onClick }) => {
-    const [categories, setCategory] = useState(["all items"]);
-    useEffect(() => {
-        const getCategories = async () => {
-            const response = await fetch(
-                "https://fakestoreapi.com/products/categories"
-            ).then((res) => res.json());
-            setCategory(["all items", ...response]);
-        };
-        getCategories();
-    }, []);
+const ItemMenu = ({ onClick, categories }) => {
     return (
         <div className="item-menu">
             {categories.map((cat, n) => {
                 return (
                     <NavLink
                         key={n}
-                        to={"/items/category/" + urlSlug(cat)}
+                        to={"/items/category/" + cat.slug}
                         className="category"
                         onClick={() => {
                             onClick(cat);
                         }}
                     >
-                        <li>{cat}</li>
+                        <li>{cat.name}</li>
                     </NavLink>
                 );
             })}
