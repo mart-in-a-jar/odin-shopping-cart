b4e2d7768b827ad17517f66f7c69404cf6d34c8a
Martin laptop <Martin.Andre.Jarneid@ssn.no>
2023-03-16 15:51:04 +0100

    Begin implementing add-to-cart-module

--- src/components/Items.js
+++ src/components/Items.js
@@ -13,8 +13,10 @@ const Items = () => {
         slug: urlSlug("all items"),
     };
 
+    const ITEMSTODISPLAYINITIALLY = 6;
+
     const [items, setItems] = useState([]);
-    const [amount, setAmount] = useState(6);
+    const [amount, setAmount] = useState(ITEMSTODISPLAYINITIALLY);
     const [currentCategory, setCurrentCategory] = useState(CATEGORIESTEMPLATE);
     const [allCategories, setAllCategories] = useState([CATEGORIESTEMPLATE]);
     const [displayLoadButton, setDisplayLoadButton] = useState(true);
@@ -70,7 +72,7 @@ const Items = () => {
             const products = await fetch(
                 shopUrl + categoryQuery + limitQuery
             ).then((result) => result.json());
-            
+
             setItems(products);
             console.log(currentCategory);
             console.log("products fetched");
@@ -87,7 +89,12 @@ const Items = () => {
         } else {
             setDisplayLoadButton(true);
         }
-    },[items, amount])
+    }, [items, amount]);
+
+    // Reset amount when category changes
+    useEffect(() => {
+        setAmount(ITEMSTODISPLAYINITIALLY);
+    }, [currentCategory]);
 
     const loadMore = () => {
         setAmount((prevAmount) => prevAmount + 3);
@@ -134,7 +141,77 @@ const ItemCard = ({ item }) => {
                 <div className="title">{item.title}</div>
             </Link>
             <div className="divider"></div>
-            <div>add to cart</div>
+            <div className="product-card-actions">
+                <div className="price">{item.price}</div>
+                <AddToCartModule item={item} />
+            </div>
+        </div>
+    );
+};
+
+const AddToCartModule = ({item}) => {
+    const [displayedAmount, setDisplayedAmount] = useState(1);
+    const [actualAmount, setActualAmount] = useState(1);
+
+    const addToCart = () => {
+        console.log(actualAmount + " * " + item.title)
+    }
+
+    const handleInput = ({ target }) => {
+        if (target.value > 0 && target.value < 100) {
+            setDisplayedAmount(+target.value);
+        } else if (target.value === "") {
+            setDisplayedAmount("");
+        }
+    };
+    const handleFocus = () => {
+        setDisplayedAmount("");
+    };
+    const handleBlur = ({ target }) => {
+        if (target.value === "") {
+            setDisplayedAmount(actualAmount);
+        }
+    };
+    useEffect(() => {
+        if (displayedAmount > 0 && displayedAmount < 100) {
+            setActualAmount(displayedAmount);
+        }
+    }, [displayedAmount]);
+
+    return (
+        <div className="add-to-cart">
+            <div className="amount-picker">
+                <button
+                    disabled={actualAmount <= 1}
+                    onClick={() => {
+                        setDisplayedAmount((prevAmount) => {
+                            return prevAmount - 1;
+                        });
+                    }}
+                >
+                    -
+                </button>
+                <input
+                    type="tel"
+                    value={displayedAmount}
+                    onChange={handleInput}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+                <button
+                    disabled={actualAmount >= 99}
+                    onClick={() => {
+                        setDisplayedAmount((prevAmount) => {
+                            return prevAmount + 1;
+                        });
+                    }}
+                >
+                    +
+                </button>
+            </div>
+            <div className="add-to-cart-button">
+                <button onClick={addToCart}>Add to cart</button>
+            </div>
         </div>
     );
 };
